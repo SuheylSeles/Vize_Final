d1b7ac7a18ce8b58ab29fd41aa437b134101bdc9
Süheyl <suheylseles@gmail.com>
2021-08-23 23:26:40 +0300

    vize ve final hesaplama eklendi.

---
+++ vize_final_hesaplama.py
@@ -0,0 +1,16 @@
+print("""
+
+ÜNİVERSİTE VİZE VE FİNAL HESAPLAMA
+
+""")
+
+
+vize1 = int(input("Vize1 notunu giriniz: "))
+vize2 = int(input("Vize2 notunu giriniz: "))
+final = int(input("Final notunu giriniz: "))
+
+
+ortalama = ((vize1*0.3) + (vize2*0.3) + (final*0.4))
+print("Ortalama Notunuz: ", ortalama)
+
+
